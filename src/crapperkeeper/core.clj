f52ae207e9cae5278d9dfde781f45b40d2fef9f6
Kevin Corcoran <kevin.corcoran@puppetlabs.com>
2015-03-17 09:25:05 -0700

    add ServiceInterface id, services now keyed on id

--- src/crapperkeeper/core.clj
+++ src/crapperkeeper/core.clj
@@ -1,32 +1,35 @@
 (ns crapperkeeper.core
   (:require [crapperkeeper.internal :as internal]
             [schema.core :as schema])
-  (:import (clojure.lang Keyword Symbol IFn)))
+  (:import (clojure.lang Keyword IFn)))
 
 (schema/defrecord ServiceInterface
-  ; For now, a service function is simply a name (keyword)
-  [service-fns :- #{Keyword}])
+  [id :- Keyword
+   service-fns :- #{Keyword}]) ; For now, a service function is simply a name (keyword)
 
 (def Service
-  {(schema/optional-key :implements)  Symbol
+  "A schema which describes a Trapperkeeper service."
+  {(schema/optional-key :implements)  ServiceInterface
    (schema/optional-key :service-fns) {Keyword IFn}})
 
 (schema/defn ^:always-validate boot!
   "Starts the Trapperkeeper framework with the given list of services."
   [& services :- [Service]]
-  (reset! internal/services services)
-  #_(let [first-service (first services)
-        service-symbol (:implements first-service)]
-    (swap! internal/services assoc service-symbol first-service)))
+  (reset! internal/services
+          (into {}
+                (for [service services]
+                  {(get-in service [:implements :id]) service}))))
 
-(defn service-call
+(schema/defn ^:always-validate service-call
   "Inovkes the function named by 'fn-key' on the service specified by
-  'service-symbol' using the given arguments."
-  [service-symbol fn-key & args]
-  (let [service (first (filter #(= (:implements %) service-symbol)
-                               @internal/services))
-        service-fn (get-in service [:service-fns fn-key])
-        context (get @internal/contexts service-symbol)]
+  'service-id' using the given arguments."
+  [service :- ServiceInterface
+   fn-key :- Keyword
+   & args]
+  (let [context (get @internal/contexts (:id service))
+        service (get @internal/services (:id service))
+        service-fn (get-in service [:service-fns fn-key])]
+    #_(println "services are" @internal/services)
     #_(println "Invoking service function" fn-key
              "on service" service
              "with context" context)
--- test/crapperkeeper/core_test.clj
+++ test/crapperkeeper/core_test.clj
@@ -5,18 +5,16 @@
   (:import (crapperkeeper.core ServiceInterface)))
 
 (def FooService
-  (ServiceInterface. #{:foo}))
+  (ServiceInterface. :foo-service #{:foo}))
 
 (def InvalidService nil)
 
-(defn schema-error? [x] (= (:type x) :schema.core/error))
-
 (deftest simplest-service-test
-  (let [service {:implements  'FooService
+  (let [service {:implements  FooService
                  :service-fns {:foo (fn [context]
                                       "hello from foo")}}]
     (boot! service))
-  (is (= (service-call 'FooService :foo)
+  (is (= (service-call FooService :foo)
          "hello from foo")))
 
 (deftest invalid-service-test
@@ -27,10 +25,10 @@
           got-expected-exception? (atom false)]
       (try+
         (boot! service)
-        (catch schema-error? _
+        (catch [:type :schema.core/error] _
           (reset! got-expected-exception? true)))
       (is @got-expected-exception?)))
 
-  ; Lots of more cases that could be added here, but we're really just
-  ; testing schema validation.
+  ; Lots of more cases that could be added here,
+  ; but we're really just testing schema validation.
   )
